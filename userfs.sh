23f5befa5847b4bd31607e5311bbd08d6615406e
F13Paul <frincupauloctav2006@gmail.com>
2025-12-21 12:32:10 +0200

    Nu mai afisam pe ecran, ci facem directorul pentru fiecare user activ si introducem intr-un nou fiser numit procs oprocesele userului respectiv

--- userfs.sh
+++ userfs.sh
@@ -2,12 +2,12 @@
 
 users=$(ps -eo user,uid | awk '$2 >= 1000 {print $1}' | grep -v "USER" | sort -u)
 
-echo "Lista utilizatori activi si procesele acestora: "
-
 for user in $users
 do
-  echo "Utilizatorul este $user"
-  echo "Procesele rulate de $user: "
-  ps -u "$user" -o pid,comm,time
+  if [ ! -d "$user" ]; then
+    mkdir "$user"
+  fi
+
+  ps -u "$user" -o pid,comm,time > "$user/procs"
 
 done
